c2f948be2495b9a55cb1bf6c857dbbd3a6c9e6c2
Scott Marlow <smarlow@redhat.com>
2014-02-12 18:50:57 -0500

    JIPI-30 add EclipseLink unit tests that check basic 2lc behaviour

---
+++ eclipselink/src/test/java/org/jipijapa/test/eclipselink/AbstractTestCase.java
@@ -0,0 +1,27 @@
+package org.jipijapa.test.eclipselink;
+
+import static org.junit.Assert.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ * AbstractTestCase
+ *
+ * @author Scott Marlow
+ */
+public class AbstractTestCase {
+
+    public EntityManagerFactory createEntityManagerFactory(String unitName) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory(unitName);
+        assertNotNull(entityManagerFactory);
+        return entityManagerFactory;
+    }
+
+    public EntityManager createEntityManager(String unitName) {
+        return createEntityManagerFactory(unitName).createEntityManager();
+
+    }
+
+}
